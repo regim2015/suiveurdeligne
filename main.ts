f9e0d67c516ccd239faee8af9a6171beb12d1238
Mohamed ben amor <mohamed.benamorsbeitla@gmail.com>
2021-04-03 12:17:10 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,29 @@
+basic.forever(function () {
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 255)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 255)
+    } else {
+        if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 0)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 255)
+            if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 0)
+                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 255)
+            }
+        } else {
+            if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 255)
+                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 0)
+            }
+            if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 255)
+                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 0)
+            }
+            if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 255)
+            } else {
+                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 0)
+            }
+        }
+    }
+})
